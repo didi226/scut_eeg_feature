77ae5be6ae972931f59439079b14fb02f43cf9d6
dididi138 <3517725675@qq.com>
2026-01-07 15:38:24 +0800

    doc aperiodic35

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@ import os
 import sys
 from pathlib import Path
 from sphinx.locale import _
-sys.path.append(str(Path(".").resolve()))
+# sys.path.append(str(Path(".").resolve()))
 sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'SCUT EEG Feature'
